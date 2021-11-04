d915fbb0cc84ae16a261e88753f17733b8d4c8ae
howkmii <abdelhakim@univ-tiaret.dz>
2021-11-04 14:05:08 +0100

    Word distance

--- Manipulating-word-embeddings.py
+++ Manipulating-word-embeddings.py
@@ -44,3 +44,41 @@ for i in range(0, len(words)):
 
 
 plt.show()
+
+
+
+
+words = ['sad', 'happy', 'town', 'village']
+
+bag2d = np.array([vec(word) for word in words]) # Convert each word to its vector representation
+
+fig, ax = plt.subplots(figsize = (10, 10)) # Create custom size image
+
+col1 = 3 # Select the column for the x axe
+col2 = 2 # Select the column for the y axe
+
+# Print an arrow for each word
+for word in bag2d:
+    ax.arrow(0, 0, word[col1], word[col2], head_width=0.0005, head_length=0.0005, fc='r', ec='r', width = 1e-5)
+    
+# print the vector difference between village and town
+village = vec('village')
+town = vec('town')
+diff = town - village
+ax.arrow(village[col1], village[col2], diff[col1], diff[col2], fc='b', ec='b', width = 1e-5)
+
+# print the vector difference between village and town
+sad = vec('sad')
+happy = vec('happy')
+diff = happy - sad
+ax.arrow(sad[col1], sad[col2], diff[col1], diff[col2], fc='b', ec='b', width = 1e-5)
+
+
+ax.scatter(bag2d[:, col1], bag2d[:, col2]); # Plot a dot for each word
+
+# Add the word label over each dot in the scatter plot
+for i in range(0, len(words)):
+    ax.annotate(words[i], (bag2d[i, col1], bag2d[i, col2]))
+
+
+plt.show()
