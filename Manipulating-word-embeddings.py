254497f08622db0dbd1319d829a20f9a13814ce3
howkmii <abdelhakim@univ-tiaret.dz>
2021-11-04 14:03:26 +0100

    we make a beautiful plot for the word embeddings of some words.

--- Manipulating-word-embeddings.py
+++ Manipulating-word-embeddings.py
@@ -17,3 +17,30 @@ print(countryVector)
 #Get the vector for a given word:
 def vec(w):
     return word_embeddings[w]
+
+
+import matplotlib.pyplot as plt # Import matplotlib
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+words = ['oil', 'gas', 'happy', 'sad', 'city', 'town', 'village', 'country', 'continent', 'petroleum', 'joyful']
+
+bag2d = np.array([vec(word) for word in words]) # Convert each word to its vector representation
+
+fig, ax = plt.subplots(figsize = (10, 10)) # Create custom size image
+
+col1 = 3 # Select the column for the x axis
+col2 = 2 # Select the column for the y axis
+
+# Print an arrow for each word
+for word in bag2d:
+    ax.arrow(0, 0, word[col1], word[col2], head_width=0.005, head_length=0.005, fc='r', ec='r', width = 1e-5)
+
+    
+ax.scatter(bag2d[:, col1], bag2d[:, col2]); # Plot a dot for each word
+
+# Add the word label over each dot in the scatter plot
+for i in range(0, len(words)):
+    ax.annotate(words[i], (bag2d[i, col1], bag2d[i, col2]))
+
+
+plt.show()
