cfa3b0033ddde4b4feab8dc75e0cf4264141d370
howkmii <abdelhakim@univ-tiaret.dz>
2021-11-04 13:59:11 +0100

    Print the values of the vector

--- Manipulating-word-embeddings.py
+++ Manipulating-word-embeddings.py
@@ -2,3 +2,13 @@
 import pandas as pd # Library for Dataframes 
 import numpy as np # Library for math functions
 import pickle # Python object serialization library. Not secure
+
+
+word_embeddings = pickle.load( open( "word_embeddings_subset.p", "rb" ) )
+len(word_embeddings) # there should be 243 words that will be used in this assignment
+
+
+
+countryVector = word_embeddings['country'] # Get the vector representation for the word 'country'
+print(type(countryVector)) # Print the type of the vector. Note it is a numpy array
+print(countryVector)
